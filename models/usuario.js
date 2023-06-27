22e2939061d6a81442f5a8b19d4b5f3c0ef35aa4
jorge151550 <117932316+jorge151550@users.noreply.github.com>
2023-06-27 08:23:11 -0500

    Update usuario.js

--- models/usuario.js
+++ models/usuario.js
@@ -6,19 +6,27 @@ const { Schema, model } = require('mongoose');
 const UsuarioSchema = Schema({
   nombre: {
     type: String,
-    required: [true, 'El nombre es obligatoria']
+    required: [true, 'El nombre es obligatoria'],
+    maxlength      : [ 50, 'El nombre no puede exceder los 50 caracteres' ],
+    minlength      : [ 3, 'El nombre debe contener 3 o más caracteres' ]
   },
   apellidos: {
     type: String,
-    required: [true, 'El apellido es obligatoria']
+    required: [true, 'El apellido es obligatoria'],
+    maxlength      : [ 50, 'El apellido no puede exceder los 50 caracteres' ],
+    minlength      : [ 3, 'El apellido debe contener 3 o más caracteres' ]
   },
   correo: {
     type: String,
-    required: [true, 'El correo es obligatoria']
+    required: [true, 'El correo es obligatoria'],
+    maxlength      : [ 20, 'El correo no puede exceder los 20 caracteres' ],
+    minlength      : [ 8, 'El correo debe contener 8 o más caracteres' ]
   },
   telefono: {
     type: Number,
-    required: [true, 'El telefono es obligatoria']
+    required: [true, 'El telefono es obligatoria'],
+    maxlength      : [ 12, 'El Telefono no puede exceder los 12 caracteres' ],
+    minlength      : [ 10, 'El Telefono debe contener 10 o más caracteres' ]
   },
   rol: {
     type: String,
@@ -36,7 +44,9 @@ const UsuarioSchema = Schema({
 },
 contrasena: {
   type: Number,
-  required: [true, 'La Contraseña es obligatoria ']
+  required: [true, 'La Contraseña es obligatoria '],
+  maxlength      : [ 10, 'La Contraseña no puede exceder los 10 caracteres' ],
+  minlength      : [ 5, 'La Contraseña no debe contener menos de 5 caracteres' ]
 }
 });
 
